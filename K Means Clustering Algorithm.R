a784cd3099daa6a154e1ecf755b70104cebc8755
Goutham Swaminathan <goutham1220@gmail.com>
2018-07-12 15:25:08 -0400

    finished initial clusters

--- K Means Clustering Algorithm.R
+++ K Means Clustering Algorithm.R
@@ -13,6 +13,11 @@ centroid2 = sample(1:100, 2)
 centroid3 = sample(1:100, 2)
 centroid4 = sample(1:100, 2)
 
+centroid1
+centroid2
+centroid3
+centroid4
+
 distance <- function(matrix, centroid){
   
   dist = vector()
@@ -29,6 +34,11 @@ distances2 = distance(dataset, centroid2)
 distances3 = distance(dataset, centroid3)
 distances4 = distance(dataset, centroid4)
 
+distances1
+distances2
+distances3
+distances4
+
 all4distances = matrix(ncol = npoints, nrow = 4)
 
 all4distances[1,] = distances1
@@ -36,18 +46,39 @@ all4distances[2,] = distances2
 all4distances[3,] = distances3
 all4distances[4,] = distances4
 
+all4distances
+
 clusterAssignment = vector()
 
 for(i in 1:npoints){
   clusterAssignment[i] = which.min(all4distances[,i])
 }
 
-cluster1 = matrix(data = NA, npoints, 2)
-cluster2 = matrix(data = NA, npoints, 2)
-cluster3 = matrix(data = NA, npoints, 2)
-cluster4 = matrix(data = NA, npoints, 2)
+clusterAssignment
+
+cluster1 = matrix(data = NA, npoints, ncol = 2)
+cluster2 = matrix(data = NA, npoints, ncol = 2)
+cluster3 = matrix(data = NA, npoints, ncol = 2)
+cluster4 = matrix(data = NA, npoints, ncol = 2)
 
 for(i in 1:npoints){
-  switch(clusterAssignment[i], clusterAssignment = 1, clusterAssignment = 2)
-}
+    if(clusterAssignment[i] == 1){
+      cluster1[i,] = dataset[i,]
+    } else if(clusterAssignment[i] == 2){
+      cluster2[i,] = dataset[i,]
+    } else if(clusterAssignment[i] == 3){
+      cluster3[i,] = dataset[i,]
+    } else if(clusterAssignment[i] == 4){
+      cluster4[i,] = dataset[i,]
+    }
+  }
 
+cluster1 = cluster1[rowSums(is.na(cluster1)) != ncol(cluster1), ]
+cluster2 = cluster2[rowSums(is.na(cluster2)) != ncol(cluster2), ]
+cluster3 = cluster3[rowSums(is.na(cluster3)) != ncol(cluster3), ]
+cluster4 = cluster4[rowSums(is.na(cluster4)) != ncol(cluster4), ]
+
+cluster1
+cluster2
+cluster3
+cluster4
