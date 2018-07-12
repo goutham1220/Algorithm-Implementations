eb90c69f4ba01c440ee6b7b7091400c0933428fe
Goutham Swaminathan <goutham1220@gmail.com>
2018-07-12 14:25:04 -0400

    updated files

--- K Means Clustering Algorithm.R
+++ K Means Clustering Algorithm.R
@@ -1,3 +1,4 @@
+set.seed(2)
 
 #Setting up the datasets
 npoints = 100
@@ -7,22 +8,46 @@ dataset[,2] = sample(1:100, npoints)
 dataset
 
 #initialize centroids to random values
-centroid1 = dataset[sample(1:100, 1),]
-centroid2 = dataset[sample(1:100, 1),]
-centroid3 = dataset[sample(1:100, 1),]
-centroid4 = dataset[sample(1:100, 1),]
+centroid1 = sample(1:100, 2)
+centroid2 = sample(1:100, 2)
+centroid3 = sample(1:100, 2)
+centroid4 = sample(1:100, 2)
 
 distance <- function(matrix, centroid){
-  length = sqrt((centroid[1] - matrix[1])^2 + (centroid[2] - matrix[2])^2)
-  return(length)
+  
+  dist = vector()
+  
+  for(i in 1:nrow(matrix)){
+    dist[i] = sqrt((centroid[1] - matrix[i,1])^2 + (centroid[2] - matrix[i,2])^2)
+  }
+  
+  return(dist)
 }
 
-clusterAssignment = matrix(data = NA, npoints, 1)
+distances1 = distance(dataset, centroid1)
+distances2 = distance(dataset, centroid2)
+distances3 = distance(dataset, centroid3)
+distances4 = distance(dataset, centroid4)
 
-clusterAssignment[,1] = which.min(c(distance(dataset[1,], centroid1), 
-                                    distance(dataset[,], centroid2), 
-                                    distance(dataset[,], centroid3), 
-                                    distance(dataset[,], centroid4)))
+all4distances = matrix(ncol = npoints, nrow = 4)
 
+all4distances[1,] = distances1
+all4distances[2,] = distances2
+all4distances[3,] = distances3
+all4distances[4,] = distances4
 
-clusterAssignment
+clusterAssignment = vector()
+
+for(i in 1:npoints){
+  clusterAssignment[i] = which.min(all4distances[,i])
+}
+
+cluster1 = matrix(data = NA, npoints, 2)
+cluster2 = matrix(data = NA, npoints, 2)
+cluster3 = matrix(data = NA, npoints, 2)
+cluster4 = matrix(data = NA, npoints, 2)
+
+for(i in 1:npoints){
+  switch(clusterAssignment[i], clusterAssignment = 1, clusterAssignment = 2)
+}
+
